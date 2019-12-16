de5bb8423039a1356c4de300d4320ab3c2dc80ef
AdamDKing <adamdentonking@gmail.com>
2019-12-16 15:18:51 -0500

    basic user input with Scanner

--- adventure/src/main/java/com/revature/adventure/AdventureApp.java
+++ adventure/src/main/java/com/revature/adventure/AdventureApp.java
@@ -1,8 +1,15 @@
 package com.revature.adventure;
 
+import java.util.Scanner;
+
 public class AdventureApp {
+	
+	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		System.out.println("Enter a number:");
+		Integer userNumber = sc.nextInt();
+		System.out.println("Here's your number: " + userNumber);
 	}
 
 }
