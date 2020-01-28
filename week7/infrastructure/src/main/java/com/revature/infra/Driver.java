b6094d0bc08c54ff0ed133e26c551a1b56817df0
AdamDKing <adamdentonking@gmail.com>
2020-01-28 14:38:12 -0500

    continued infra, setter + constructor injection

--- week7/infrastructure/src/main/java/com/revature/infra/Driver.java
+++ week7/infrastructure/src/main/java/com/revature/infra/Driver.java
@@ -1,5 +1,10 @@
 package com.revature.infra;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.revature.infra.beans.Cabin;
 import com.revature.infra.beans.House;
 import com.revature.infra.beans.WaterTower;
 
@@ -7,6 +12,7 @@ public class Driver {
 	
 	public static void main(String[] args) {
 		//Let's off with manual config in Java, NO SPRING
+		/*
 		WaterTower wt = new WaterTower();
 		wt.setResource("water");
 		House adamsHouse = new House();
@@ -14,6 +20,40 @@ public class Driver {
 		adamsHouse.setWaterTower(wt);
 		
 		adamsHouse.checkStatus();
+		*/
+		
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		
+		WaterTower wtBean = (WaterTower) ac.getBean("waterTower");
+		//another option:
+		//WaterTower wtBeanOption = ac.getBean(WaterTower.class);
+		
+		//true because singleton scope:
+		//System.out.println(wtBean == wtBeanOption);
+		
+		House adamsHouse = ac.getBean("house", House.class);
+		
+		House adamsSummerHome = ac.getBean("house", House.class);
+		
+
+		adamsHouse.setName("Adam House");
+		adamsSummerHome.setName("Summer Home");
+		adamsHouse.checkStatus();
+		adamsSummerHome.checkStatus();
+		
+		Cabin adamsCabin = ac.getBean("cabin", Cabin.class);
+		Cabin adamsSummerCabin = ac.getBean("cabin", Cabin.class);
+		
+		adamsCabin.setName("Adam Cabin");
+		adamsSummerCabin.setName("Adam Summer Cabin");
+		
+		adamsCabin.checkStatus();
+		adamsSummerCabin.getWell().setResource("Lava");
+		adamsSummerCabin.checkStatus();
+		
+		//to close we need to cast:
+		((ConfigurableApplicationContext) ac).close();
+		
 	}
 
 }
