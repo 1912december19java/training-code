98c0daba2a34daa33433183fa0d34a58559e350a
AdamDKing <adamdentonking@gmail.com>
2020-01-31 14:53:58 -0500

    added CORS filter

--- week7/books/src/main/java/com/revature/books/controllers/AuthorController.java
+++ week7/books/src/main/java/com/revature/books/controllers/AuthorController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.revature.books.services.ModelService;
 //This is just @Controller and @ResponseBody combined
 @RestController
 @RequestMapping("/authors")
+@CrossOrigin
 public class AuthorController implements InitializingBean {
 	
 	@Override
