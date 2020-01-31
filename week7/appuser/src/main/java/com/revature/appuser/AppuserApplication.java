2558f9c44b1a9f50c31ff14fe178c83b1b045f0a
AdamDKing <adamdentonking@gmail.com>
2020-01-31 11:05:36 -0500

    appuser with validation

--- week7/appuser/src/main/java/com/revature/appuser/AppuserApplication.java
+++ week7/appuser/src/main/java/com/revature/appuser/AppuserApplication.java
@@ -2,6 +2,9 @@ package com.revature.appuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AppuserApplication {
@@ -10,4 +13,14 @@ public class AppuserApplication {
 		SpringApplication.run(AppuserApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "OPTIONS", "PUT", "POST", "PATCH", "DELETE");
+			}
+		};
+
+	}
 }
